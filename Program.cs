dac020b7745c3a65113d22aad795ab0636cff680
ilyaTATARENTSEV <117466522+tatarentsev@users.noreply.github.com>
2023-02-06 19:41:37 +0300

    №50

--- Program.cs
+++ Program.cs
@@ -1,12 +1,13 @@
-﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
 // 8 4 2 4
-// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+// 1 7 -> такого числа в массиве нет
+// 1 1 -> 9
 
-int l = 3;
-int k = 4;
+int l = 4;
+int k = 3;
 
 int[,] array = new int[l, k];
 
@@ -15,26 +16,30 @@ for (int i = 0; i < array.GetLength(0); i++)
     for (int j = 0; j < array.GetLength(1); j++)
     {
         array[i, j] = new Random().Next(0, 10);
-    }   
+    }    
 }
-
 
 void PrintArray(int[,] arr)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+   for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            Console.Write($"{arr[i, j]}  ");
+            Console.Write($"{arr[i, j]} \t");
         }
         Console.WriteLine();
-    }
+    } 
+}
+
+Console.Write("Задайте координату n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте координату m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+
+if (n < l & m < k)
+{
+    Console.WriteLine(array[n, m]);
 }
 
 PrintArray(array);
-
-Console.WriteLine($"Сумма первого столбца равняется: {array[0,0] + array[1,0] + array[2,0]}");
-Console.WriteLine($"Сумма второго столбца равняется: {array[0,1] + array[1,1] + array[2,1]}");
-Console.WriteLine($"Сумма третьего столбца равняется: {array[0,2] + array[1,2] + array[2,2]}");
-Console.WriteLine($"Сумма последнего столба равняется: {array[0,3] + array[1,3] + array[2,3]}");
-Console.ReadLine();
