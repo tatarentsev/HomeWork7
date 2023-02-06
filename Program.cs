aec2aa93d9fbf2beb2be6e235fd67d9be802ba1a
ilyaTATARENTSEV <117466522+tatarentsev@users.noreply.github.com>
2023-02-06 19:39:54 +0300

    Homework semminar 7

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int l = 3;
+int k = 4;
+
+int[,] array = new int[l, k];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(0, 10);
+    }   
+}
+
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray(array);
+
+Console.WriteLine($"Сумма первого столбца равняется: {array[0,0] + array[1,0] + array[2,0]}");
+Console.WriteLine($"Сумма второго столбца равняется: {array[0,1] + array[1,1] + array[2,1]}");
+Console.WriteLine($"Сумма третьего столбца равняется: {array[0,2] + array[1,2] + array[2,2]}");
+Console.WriteLine($"Сумма последнего столба равняется: {array[0,3] + array[1,3] + array[2,3]}");
+Console.ReadLine();
---
+++ Program47.cs
@@ -0,0 +1,32 @@
+﻿//  Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+int m = 3; 
+int n = 4;
+
+double[,] arr = new double[m, n];
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray(arr);
